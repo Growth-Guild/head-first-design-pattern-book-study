2b93a7c68b82ae0a7a973544273bb1cc1b07a8ee
icarus8050 <icarus8050@naver.com>
2022-06-19 13:02:53 +0900

    팩토리 메서드 패턴 예제

---
+++ src/main/java/chapter_4/factory_method_pattern/GreekPizza.java
@@ -0,0 +1,23 @@
+package chapter_4.factory_method_pattern;
+
+public class GreekPizza extends Pizza {
+    @Override
+    public void prepare() {
+
+    }
+
+    @Override
+    public void bake() {
+
+    }
+
+    @Override
+    public void cut() {
+
+    }
+
+    @Override
+    public void box() {
+
+    }
+}
